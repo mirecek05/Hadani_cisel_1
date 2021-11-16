075fc5dee5b83489796c1b993ae5da399970c74e
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2021-11-16 10:07:50 +0100

    Přidejte soubory projektu.

---
+++ Hadani_cisel_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hadani_cisel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
